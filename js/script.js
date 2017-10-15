59e6e814c410f60396ce1614e2d88e208d4c38d1
Dan <daniel.munozdev@gmail.com>
2017-10-14 21:21:28 -0700

    Alert sum of inputted variables

--- js/script.js
+++ js/script.js
@@ -16,3 +16,6 @@ function divide(number1, number2) {
 
 let number1 = parseFloat(prompt('Enter a number: '));
 let number2 = parseFloat(prompt('Enter another number: '));
+
+let result = add(number1, number2);
+alert(result);
