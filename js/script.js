fe7d380afdaca60ebd0a5eac0c65170064c3c76a
Dan <daniel.munozdev@gmail.com>
2017-10-14 21:20:42 -0700

    Prompt user for two numbers and store input into variables

--- js/script.js
+++ js/script.js
@@ -13,3 +13,6 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
+
+let number1 = parseFloat(prompt('Enter a number: '));
+let number2 = parseFloat(prompt('Enter another number: '));
