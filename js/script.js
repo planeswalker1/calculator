0ba2b0379f2c69e1b468fbce03fbeac1364cd968
Dan <daniel.munozdev@gmail.com>
2017-10-15 11:46:01 -0700

    Remove prompts and alert; Add submit event listener to form

--- js/script.js
+++ js/script.js
@@ -16,8 +16,7 @@ function divide(number1, number2) {
 }
 
 //Everything below this line is user interface (or front-end) logic:
-let number1 = parseFloat(prompt('Enter a number: '));
-let number2 = parseFloat(prompt('Enter another number: '));
-
-let result = add(number1, number2);
-alert(result);
+$('form#add').submit(function(event) {
+  event.preventDefault();
+  alert('form submitted');
+});
