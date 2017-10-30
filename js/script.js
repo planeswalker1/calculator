aec0ceec89f29ec828bde94d048580c9709fdf7c
Dan <daniel.munozdev@gmail.com>
2017-10-29 19:51:05 -0700

    Remove submit event slisteners

--- js/script.js
+++ js/script.js
@@ -16,34 +16,3 @@ function divide(number1, number2) {
 }
 
 //Everything below this line is user interface (or front-end) logic:
-$('form#add').submit(function(event) {
-  event.preventDefault();
-  let addInput1 = parseFloat($('input#add1').val());
-  let addInput2 = parseFloat($('input#add2').val());
-  let result = add(addInput1, addInput2);
-  $('p#addOutput').text(result);
-});
-
-$('form#subtract').submit(function(event) {
-  event.preventDefault();
-  let subtractInput1 = parseFloat($('input#subtract1').val());
-  let subtractInput2 = parseFloat($('input#subtract2').val());
-  let result = subtract(subtractInput1, subtractInput2);
-  $('p#subtractOutput').text(result);
-});
-
-$('form#multiply').submit(function(event) {
-  event.preventDefault();
-  let multiplyInput1 = parseFloat($('input#multiply1').val());
-  let multiplyInput2 = parseFloat($('input#multiply2').val());
-  let result = multiply(multiplyInput1, multiplyInput2);
-  $('p#multiplyOutput').text(result);
-});
-
-$('form#divide').submit(function(event) {
-  event.preventDefault();
-  let divideInput1 = parseFloat($('input#divide1').val());
-  let divideInput2 = parseFloat($('input#divide2').val());
-  let result = divide(divideInput1, divideInput2);
-  $('p#divideOutput').text(result);
-});
